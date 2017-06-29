0888d201b483b71239c7039a276efdf1fc8f4435
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-06-29 02:20:46 -0700

    Finished HTML labeling and formatting, created submit button for the user info input for train that will push the data to firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
-// Initializing Firebase to a Train Scheduler database
+//This progam will grab user input and append to the table, ID "train-list"
+//Initializing Firebase to a Train Scheduler database
 var config = {
   apiKey: "AIzaSyB5lbwpv7jwNVV00A4PBPZM4pIcGnW1V7E",
   authDomain: "train-scheduler-7ecb6.firebaseapp.com",
@@ -10,4 +11,26 @@ var config = {
 
 firebase.initializeApp(config);
 var database = firebase.database();
+
+//Grabs and appends the user info for train details when they click "Submit" at bottom
+$("#submit-button").on('click', function(){
+  var newName = $('#train-name').val().trim();
+  var newDestination = $('#destination').val().trim();
+  var newTrainTime = $('#first-train-time').val().trim();
+  var newFrequency = $('#frequency').val().trim();
+
+  //This pushes (AKA uploads) user info for train details into the firebase cloud
+  database.ref().push({
+    name: newName,
+    destination: newDestination,
+    firstTrainTime: newTrainTime,
+    frequency: newFrequency,
+    startedAt: firebase.database.ServerValue.TIMESTAMP
+  })
+
+  //Helps keep track of our push to the cloud
+  console.log('attempt to push')
+  //Allows user to input values for train info; clears the text boxes
+  $('input').val('')
+})
 

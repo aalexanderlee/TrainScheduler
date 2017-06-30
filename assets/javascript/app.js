63449f6bb92ca9b6408abed43041fc7785a3baa0
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-06-29 22:08:33 -0700

    The displays for the arrival times and minutes left were not appending correctly early on, this was just fixed with append commands after using snap.val() data to define our output

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,55 +15,72 @@ var database = firebase.database();
 
 //Grabs and appends the user info for train details when they click "Submit" at bottom
 $("#submit-button").on('click', function(){
-  var newName = $('#train-name').val().trim(); //given to append
-  var newDestination = $('#destination').val().trim(); //given to append
-  var newFrequency = $('#frequency').val().trim(); //given to append and used for calculations
-  var newArrival = $('#new-arrival').val().trim(); //used for calculations
+  var newName = $("#train-name").val().trim(); //given to append
+  var newDestination = $("#destination").val().trim(); //given to append
+  var firstArrivalUnix = moment($("#first-arrival").val().trim(), "HH:mm").subtract(10, "years").format("X"); 
+  var newFrequency = $("#frequency").val().trim(); //given to append and used for calculations
+  
 
   //This provides a path and pushes user info for train details into the firebase cloud
   database.ref().push({
     name: newName,
     destination: newDestination,
-    arrival: newArrival,
+    firstTrain: firstArrivalUnix,
     frequency: newFrequency,
     startedAt: firebase.database.ServerValue.TIMESTAMP
   })
 
   //Helps keep track of our push to the cloud
-  console.log('attempt to push')
+  console.log(snapshot.val().name);
+  console.log(snapshot.val().destination);
+  console.log(firstArrivalUnix);
+  console.log(snapshot.val().frequency);
+
+  alert("All variables has been added to Firebase.")
+
   //Allows user to input values for train info; clears the text boxes
-  $('input').val('')
+  $("#train-name").val("");
+  $("#destination").val("");
+  $("first-arrival").val("");
+  $("#frequency").val("");
+
 })
 
 //listen for a new child
-database.ref().on('child_added', function(snap){
+database.ref().on("child_added", function(snapshot){
 
-  console.log(snap.val())
+  console.log(snapshot.val());
   
   //Display all user given inputs (train name, destination, frequency)
   var trainInfo = $('<tr>');
-  var displayName = $('<td>').append(snap.val().name);
-  var displayDestination = $('<td>').append(snap.val().destination);
-  var displayFrequency = $('<td>').append(snap.val().frequency);
+
+  var displayName = $('<td>').append(snapshot.val().name);
+  var displayDestination = $('<td>').append(snapshot.val().destination);
+  var displayFrequency = $('<td>').append(snapshot.val().frequency);
+  var unixFirstArrival = snapshot.val().firstTrain;
 
   //Calculate the difference in time to find arrival with frequency data stored in Firebase
-  var timeDiff = moment().diff(moment.unix(snap.val().arrival), "minutes");
-  var timeLeft = moment().diff(moment.unix(snap.val().arrival), "minutes") % (snap.val().frequency);
+  var timeDiff = moment().diff(moment.unix(unixFirstArrival), "minutes");
+  var timeLeft = moment().diff(moment.unix(unixFirstArrival), "minutes") % (snapshot.val().frequency);
   
-  var minutes = newFrequency - timeLeft;
+  var minutes = (snapshot.val().frequency) - timeLeft;
   var nextArrival = moment().add(minutes, "m").format("hh:mm A"); 
 
-  //Display next arrival
-  var displayNextArrival = $('<td>').append(nextArrival);
-  //Display minutes until arrival from moment
+  var displayArrival = $('<td>').append(nextArrival);
   var displayMinutes = $('<td>').append(minutes);
+  //Display next arrival
+  //var displayNextArrival = $('<td>').append(nextArrival);
+  //Display minutes until arrival from moment
+  //var displayMinutes = $('<td>').append(minutes);
 
-  trainInfo.append(displayName, displayDestination, displayFrequency, displayNextArrival, displayMinutes);
+  trainInfo.append(displayName, displayDestination, displayFrequency, displayArrival, displayMinutes);
   $('#train-list').append(trainInfo);
 
 }, function(err){
   console.log("Errors: " + err.code);
   //alert(err.code)
   });
+
+clear();
 
 });
